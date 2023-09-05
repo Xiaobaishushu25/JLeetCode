3161a2fc1001637e469fb2055515a7af9ab7e108
Xbss <2055761346@qq.com>
2023-09-05 16:27:46 +0800

    移动0

--- easy/src/移动零/移动零.kt
+++ easy/src/移动零/移动零.kt
@@ -1,16 +1,44 @@
 package 移动零
 
+/**
+ * 给定一个数组 nums，编写一个函数将所有 0 移动到数组的末尾，同时保持非零元素的相对顺序。
+ * 请注意 ，必须在不复制数组的情况下原地对数组进行操作。
+ * 示例 1:
+ * 输入: nums = [0,1,0,3,12]
+ * 输出: [1,3,12,0,0]
+ * 示例 2:
+ * 输入: nums = [0]
+ * 输出: [0]
+ *  https://leetcode.cn/problems/move-zeroes/description/?envType=study-plan-v2&envId=top-100-liked
+ */
 fun main() {
-
+//    moveZeroes(listOf<Int>(1,0,21,5,0,0,4,7,8,9,0,0).toIntArray())
+    moveZeroes(listOf<Int>(0,1,0,3,12).toIntArray())
 }
+
+/**
+ * 现遍历给定数组，遇到零计数器+1，不是零将当前元素前移计数器个位置，最后将数组最后计数器个元素置0
+ * 时间
+ * 464ms 击败 6.42%使用 Kotlin 的用户
+ * 内存
+ * 38.31MB 击败 50.46%使用 Kotlin 的用户
+ * @param nums
+ */
 fun moveZeroes(nums: IntArray): Unit {
-    val zero = mutableListOf<Int>()
-    for((index,value) in nums.withIndex()){
-        if (value==0){
-            zero.add(index)
-        }
+    var zeroCount = 0
+    for ((index,value) in nums.withIndex()){
+        if (value==0)
+            zeroCount++
+        else
+            moveItem(index,zeroCount,nums)
     }
-    for ((index,value) in zero.withIndex()){
-        for (i in 0)
+    var lastZero = nums.size-1
+    for (i in 1..zeroCount){
+        nums[lastZero--] = 0
     }
+    nums.forEach { print(" $it") }
+}
+fun moveItem(begin:Int,num:Int,nums:IntArray){
+//    println("正在将$begin 的元素${nums[begin]}移到${begin-num}的位置上-${nums[begin-num]}")
+    nums[begin-num] = nums[begin]
 }
